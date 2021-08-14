98b88bf1ff168287df86a11e20afe19ffc781db6
Maria Luiza Castelar <mluiza.vcr@gmail.com>
2021-08-14 19:04:41 -0300

    Organiza script

--- script.js
+++ script.js
@@ -1,16 +1,3 @@
-function adicionaLinha(linha) {
-  let table = document.getElementById("myTableBody");
-  let row = table.insertRow(0);
-  let cell1 = row.insertCell(0);
-  let cell2 = row.insertCell(1);
-  let cell3 = row.insertCell(2);
-  let cell4 = row.insertCell(3);
-
-  cell1.innerHTML = `<p>${linha.codigo}</p>`
-  cell2.innerHTML = `<p>${linha.descricao}</p>`
-  cell3.innerHTML = `<p>${linha.valor}</p>`
-  cell4.innerHTML = `<p>${linha.data}</p>`
-}
 
 let jsonLinha1 = {
   codigo: "PED00167",
@@ -52,6 +39,20 @@ let jsonLinha6 = {
   descricao: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
   valor: "R$ 70,00",
   data: "20/07/2021"
+}
+
+function adicionaLinha(linha) {
+  let table = document.getElementById("myTableBody");
+  let row = table.insertRow(0);
+  let cell1 = row.insertCell(0);
+  let cell2 = row.insertCell(1);
+  let cell3 = row.insertCell(2);
+  let cell4 = row.insertCell(3);
+
+  cell1.innerHTML = `<p>${linha.codigo}</p>`
+  cell2.innerHTML = `<p>${linha.descricao}</p>`
+  cell3.innerHTML = `<p>${linha.valor}</p>`
+  cell4.innerHTML = `<p>${linha.data}</p>`
 }
 
 adicionaLinha(jsonLinha1);
